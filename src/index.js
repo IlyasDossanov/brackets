a84346d6e781e7014f81a36145258ad76939e590
Ilyas <ilyasdossanov@gmail.com>
2021-03-11 22:21:39 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
-  let roundBracket=0, squereBracket=0, braches = 0;
+  let roundBracket=0, squereBracket=0, braches = 0, strightBracket = 0;
+  let isRight = false;
   for(let i = 0; i<str.length; i++){
     if(str.charAt(i) === '('){roundBracket+=1;}
     else if(str.charAt(i)===')'){roundBracket-=1}
@@ -7,12 +8,13 @@ module.exports = function check(str, bracketsConfig) {
     else if(str.charAt(i)===']'){squereBracket-=1}
     else if(str.charAt(i)==='{'){braches+=1}
     else if(str.charAt(i)==='}'){braches-=1}
+    else if(str.charAt(i)==='|'){strightBracket+=1}
   }
-  if(roundBracket === 0 && squereBracket === 0 && braches === 0){
-    bracketsConfig = true
+  if(roundBracket === 0 && squereBracket === 0 && braches === 0 && strightBracket%2 === 0){
+    isRight = true
   }
   else{
-    bracketsConfig = false
+    isRight = false
   }
-  return(bracketsConfig)
+  return(isRight)
 }
